bc6d5410d57503157408749038361dba9671869a
محمد <Mohamed.Hesham_taha20110377@yahoo.com>
2016-02-21 16:36:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/two_one_a_tv/MainActivity.java
@@ -0,0 +1,204 @@
+package com.example.two_one_a_tv;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+
+public class MainActivity extends ActionBarActivity   {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment /*implements SurfaceHolder.Callback, MediaPlayer.OnPreparedListener, MediaController.MediaPlayerControl */{
+      VideoView life_vv;
+  /*private MediaPlayer mediaPlayer;
+        private SurfaceHolder vidHolder;
+        private SurfaceView vidSurface;
+        private MediaController mediaController;
+        private Handler handler = new Handler();
+        String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+    */
+  String vidAddress = "http://lb218.cdn.mangomolo.com/218/smil:218.smil/playlist.m3u8";
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            life_vv=(VideoView)rootView.findViewById(R.id.life_stream);
+           // String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+            Uri vidUri = Uri.parse(vidAddress);
+
+           life_vv.setVideoURI(vidUri);
+            MediaController vidControl = new MediaController(getActivity());
+            vidControl.setAnchorView(life_vv);
+            life_vv.setMediaController(vidControl);
+
+            life_vv.start();
+            /*
+            vidSurface = (SurfaceView)rootView.findViewById(R.id.surfView);
+            vidHolder = vidSurface.getHolder();
+            vidHolder.addCallback(this);*/
+            return rootView;
+        }
+
+      /*  @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                mediaPlayer = new MediaPlayer();
+                mediaController = new MediaController(getActivity());
+                mediaPlayer.setDisplay(vidHolder);
+                mediaPlayer.setDataSource(vidAddress);
+                mediaPlayer.prepare();
+                mediaPlayer.setOnPreparedListener(this);
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
+
+        @Override
+        public void onPrepared(MediaPlayer mp) {
+            mediaPlayer.start();
+            handler.post(new Runnable() {
+                public void run() {
+                    mediaController.setEnabled(true);
+                    mediaController.show();
+                }
+            });
+        }
+
+        @Override
+        public void start() {
+       mediaPlayer.start();
+        }
+
+        @Override
+        public void pause() {
+        mediaPlayer.pause();
+        }
+
+        @Override
+        public int getDuration() {
+            return mediaPlayer.getDuration();
+        }
+
+        @Override
+        public int getCurrentPosition() {
+            return mediaPlayer.getCurrentPosition();
+        }
+
+        @Override
+        public void seekTo(int pos) {
+         mediaPlayer.seekTo(pos);
+        }
+
+        @Override
+        public boolean isPlaying() {
+            return mediaPlayer.isPlaying();
+        }
+
+        @Override
+        public int getBufferPercentage() {
+            return 0;
+        }
+
+        @Override
+        public boolean canPause() {
+            return true;
+        }
+
+        @Override
+        public boolean canSeekBackward() {
+            return false;
+        }
+
+        @Override
+        public boolean canSeekForward() {
+            return true;
+        }
+
+        @Override
+        public int getAudioSessionId() {
+            return 0;
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            mediaController.hide();
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+        public boolean onTouchEvent(MotionEvent event) {
+            //the MediaController will hide after 3 seconds - tap the screen to make it appear again
+            mediaController.show();
+            return false;
+        }*/
+
+    }
+}
